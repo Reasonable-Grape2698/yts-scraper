f046c8153084c0b7816aa90ab2de0ee4d4b468d3
Reasonable-Grape2698 <dyl.secure@gmail.com>
2025-01-05 13:24:07 +1100

    Update main.py

--- yts_scraper/main.py
+++ yts_scraper/main.py
@@ -146,6 +146,14 @@ def main():
                         const=1,
                         nargs='?')
 
+    parser.add_argument('-l, '--language',
+                        help='Enter language',
+                        dest='language',
+                        type=str.lower,
+                        required=False,
+                        default="",
+                        const="",
+                        nargs='?')
 
     try:
         args = parser.parse_args()
